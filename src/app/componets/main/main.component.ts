b6bc77cd64280bf35817edc07a5719895d583343
Yura Petrukha <yrapetruha0903@gmail.com>
2023-06-07 13:02:48 +0300

    V1
    V1

---
+++ src/app/componets/main/main.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
+import Parallax from 'parallax-js';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  ngOnInit():void {
+    var scene:any = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene);
+  }
+}
